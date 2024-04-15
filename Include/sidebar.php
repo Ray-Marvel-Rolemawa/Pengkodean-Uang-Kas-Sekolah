3e03d6b97545377cbbadd73e709d1552fbbf77af
Ray-Marvel-Rolemawa <152137665+Ray-Marvel-Rolemawa@users.noreply.github.com>
2024-04-15 20:48:31 +0700

    Update sidebar.php

--- Include/sidebar.php
+++ Include/sidebar.php
@@ -10,7 +10,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
   <a href="index.php" class="brand-link">
-    <img src="assets/img/img_properties/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <img src="assets/img/img_properties/logo.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">UANG KAS</span>
   </a>
 
