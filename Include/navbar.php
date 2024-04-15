1aba1190eefaed1c4901754325d4f770c22a98f0
Ray-Marvel-Rolemawa <152137665+Ray-Marvel-Rolemawa@users.noreply.github.com>
2024-04-15 22:04:06 +0700

    Update navbar.php

--- Include/navbar.php
+++ Include/navbar.php
@@ -1,5 +1,5 @@
 <!-- Navbar -->
-<nav class="main-header navbar navbar-expand navbar-cream navbar-dark">
+<nav class="main-header navbar navbar-expand navbar-white navbar-red">
   <!-- Left navbar links -->
   <ul class="navbar-nav">
     <li class="nav-item">
